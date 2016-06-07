30dc71cfaaeb9de9c48b366c2062b455d73cae06
Matt Colman <matt.colman@fantasycentre.com>
2016-06-07 15:04:20 +1000

    up to font-awesome

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './app.css'
+import styles from './styles.module.css'
 
 const App = React.createClass({
   render() {
-    return (<div>Text text text</div>)
+    return (
+      <div className={styles.wrapper}>
+        <h1>Environment: {__NODE_ENV__}</h1>
+      </div>
+    )
   }
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
+const NODE_ENV = process.env.NODE_ENV;
+const dotenv = require('dotenv');
+const isDev = NODE_ENV === 'development';
+// alternatively, we can use process.argv[1]
+// const isDev = (process.argv[1] || '')
+//                .indexOf('hjs-dev-server') !== -1;
+
+// ...
+//
 const webpack = require('webpack');
 const fs      = require('fs');
 const path    = require('path'),
@@ -11,18 +20,68 @@ const src     = join(root, 'src');
 const modules = join(root, 'node_modules');
 const dest    = join(root, 'dist');
 
-const NODE_ENV = process.env.NODE_ENV;
-const isDev = NODE_ENV === 'development';
-// alternatively, we can use process.argv[1]
-// const isDev = (process.argv[1] || '')
-//                .indexOf('hjs-dev-server') !== -1;
-
-// ...
 var config = getConfig({
-  isDev: true,
+  isDev: isDev,
   in: join(src, 'app.js'),
   out: dest,
   clearBeforeBuild: true
+})
+
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
+});
+const envVariables =
+    Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines =
+  Object.keys(envVariables)
+  .reduce((memo, key) => {
+    const val = JSON.stringify(envVariables[key]);
+    memo[`__${key.toUpperCase()}__`] = val;
+    return memo;
+  }, {
+    __NODE_ENV__: JSON.stringify(NODE_ENV)
+  });
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins);
+// END ENV variables
+//
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+  const found = loaders.filter(l => l &&
+      l.loader && l.loader.match(match));
+  return found ? found[0] : null;
+}
+// existing css loader
+const cssloader =
+  findLoader(config.module.loaders, matchCssLoaders);
+
+const newloader = Object.assign({}, cssloader, {
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader
+    .replace(matchCssLoaders,
+    `$1$2?modules&localIdentName=${cssModulesNames}$3`)
+})
+config.module.loaders.push(newloader);
+cssloader.test =
+  new RegExp(`[^module]${cssloader.test.source}`)
+cssloader.loader = newloader.loader
+
+
+
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
 })
 
 module.exports = config;
