9ce769d7baa797491c33c21b8323fe0e8ac9bc14
Matt Colman <matt.colman@fantasycentre.com>
2016-06-07 15:58:25 +1000

    up to Routing to Maps

--- src/app.js
+++ src/app.js
@@ -8,17 +8,9 @@ import {browserHistory, Router, Route} from 'react-router'
 
 import App from 'containers/App/App'
 
-const Home = React.createClass({
-  render() {
-    return (<div>Hello world</div>)
-  }
-})
-const routes = (
-  <Router>
-    <Route path="/" component={Home} />
-  </Router>
-)
+import makeRoutes from 'routes'
 
+const routes = makeRoutes()
 const mountNode = document.querySelector('#root');
 ReactDOM.render(
   <App history={browserHistory} routes={routes} />, mountNode);
