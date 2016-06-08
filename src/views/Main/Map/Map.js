99a53f23c3a692c97cb59cd289f71f26a77c6766
Matt Colman <matt.colman@fantasycentre.com>
2016-06-08 11:31:57 +1000

    oops I forgot to export default!! revert back to current map.js

--- src/views/Main/Map/Map.js
+++ src/views/Main/Map/Map.js
@@ -5,7 +5,8 @@ import Map, {GoogleApiWrapper, Marker} from 'google-maps-react'
 import styles from './styles.module.css'
 
 export class MapComponent extends React.Component {
-  _renderMarkers() {
+
+  renderMarkers() {
     if (!this.props.places) {
       return;
     }
@@ -21,46 +22,14 @@ export class MapComponent extends React.Component {
     });
   }
 
-  _renderChildren() {
-    const {children} = this.props;
-
-    if (React.Children.count(children) > 0) {
-      return React.Children.map(children, c => {
-        return React.cloneElement(c, this.props, {
-          map: this.props.map,
-          google: this.props.google
-        })
-      })
-    } else {
-      return this._renderMarkers();
-    }
-  }
-
   render() {
-    const {children} = this.props;
-
     return (
-      <Map map={this.props.map}
-        google={this.props.google}
-        className={styles.map}
-        zoom={this.props.zoom}
-        onRecenter={this.props.onMove}
-        onDragend={this.props.onMove}
-        onClick={this.props.onClick}
-        visible={!children || React.Children.count(children) == 0}
-        >
-        {this._renderChildren()}
+      <Map google={this.props.google}
+            className={styles.map}>
+        {this.renderMarkers()}
       </Map>
     )
   }
-}
-
-MapComponent.propTypes = {
-  onMarkerClick: T.func
-}
-const identity = (...a) => a;
-MapComponent.defaultProps = {
-  onMarkerClick: identity
 }
 
 export default MapComponent
